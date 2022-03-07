2a2ce64ef890ee9e84491196a018dbe8a0369a4f
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2022-03-07 23:23:31 +0530

    Localize image comments title
    (cherry picked from commit 7fb5a50289d4df44f6af0e05e5fd4235a606d378)

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedLocalizationTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedLocalizationTests.swift
@@ -13,6 +13,7 @@ class FeedLocalizationTests: XCTestCase {
     func test_localizedStrings_haveKeysAndValuesForAllSupportedLocalizations() {
         let table = "Feed"
         let bundle = Bundle(for: FeedPresenter.self)
+        
         assertLocalizedKeyAndValuesExist(in: bundle, table)
     }
     
