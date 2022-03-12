7e87bd593ffc77ba2afb17da4adaedccae05fcb2
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2022-03-12 12:47:55 +0530

    Implement Image Comments UI

---
+++ EssentialFeed/EssentialFeediOS/Image Comments UI/Views/ImageCommentCell.swift
@@ -0,0 +1,14 @@
+//
+//  ImageCommentCell.swift
+//  EssentialFeediOS
+//
+//  Created by Hitesh on 12/03/22.
+//
+
+import UIKit
+
+public final class ImageCommentCell: UITableViewCell {
+    @IBOutlet public var messageLabel: UILabel!
+    @IBOutlet public var usernameLabel: UILabel!
+    @IBOutlet public var dateLabel: UILabel!
+}
