0e2a5c6863147c4bf6a8e6236f4fad02c6335533
Hitesh Chotaliya <hiteshmistri08@gmail.com>
2022-04-09 10:57:33 +0530

    Add "Load more" cell controller with loading indicator

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/LoadMoreCell.swift
@@ -0,0 +1,37 @@
+//
+//  LoadMoreCell.swift
+//  EssentialFeediOS
+//
+//  Created by Hitesh on 09/04/22.
+//
+
+import UIKit
+
+public final class LoadMoreCell: UITableViewCell {
+    
+    private lazy var spinner: UIActivityIndicatorView = {
+        let spinner = UIActivityIndicatorView(style: .medium)
+        contentView.addSubview(spinner)
+        
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            spinner.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+            spinner.centerYAnchor.constraint(equalTo: contentView.centerYAnchor),
+            contentView.heightAnchor.constraint(lessThanOrEqualToConstant: 40)
+        ])
+        
+        return spinner
+    }()
+    
+    public var isLoading: Bool {
+        get { spinner.isAnimating }
+        set {
+            if newValue {
+                spinner.startAnimating()
+            } else {
+                spinner.stopAnimating()
+            }
+        }
+    }
+    
+}
